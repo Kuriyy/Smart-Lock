7ae01eaa4deebb9fc7e670aa730ecdd1df58569c
Robert <robert.pressl@kabsi.at>
2023-05-31 10:45:00 +0200

    Add Read and Write

---
+++ Programm/Read.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#coding utf-8
+#Robert Pressl
+#V1
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+finally:
+        GPIO.cleanup()
