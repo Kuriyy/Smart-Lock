f501a7fe8f6b753d1d1281675d3003f69fe087d2
Robert <robert.pressl@kabsi.at>
2023-06-01 10:39:35 +0200

    Rename and change Write

---
+++ Programm/Write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#coding utf-8
+#Robert Pressl
+#V1
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+        text = input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
+finally:
+        GPIO.cleanup()
