6b2109de4040d89d3595461b573daafa53cdd76c
Robert <robert.pressl@kabsi.at>
2023-06-01 11:56:08 +0200

    Added LEDS

--- Programm/Read.py
+++ Programm/Read.py
@@ -8,13 +8,25 @@ from mfrc522 import SimpleMFRC522
 import time
 import datetime
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.OUT)
+GPIO.output(22, GPIO.HIGH)
 
 reader = SimpleMFRC522()
 while True:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(17, GPIO.OUT)
+        GPIO.setup(22, GPIO.OUT)
         try:
                 id, text = reader.read()
                 timestamp = datetime.datetime.now()
                 print(str(id) + " " + str(text) + " " + str(timestamp))
+
         finally:
-                time.sleep(2)
+                GPIO.output(17, GPIO.HIGH)
+                GPIO.output(22, GPIO.LOW)
+                time.sleep(4)
+                GPIO.output(17, GPIO.LOW)
+                GPIO.output(22, GPIO.HIGH)
                 GPIO.cleanup()
+
