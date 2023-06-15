ba4ee8504b791535130fbcd44df3f990ba9f4a2c
Robert <robert.pressl@kabsi.at>
2023-06-15 12:05:43 +0200

    d

--- Programm/Read.py
+++ Programm/Read.py
@@ -39,8 +39,8 @@ try:
         # Lese die ID und den Text vom RFID-Lesegerät
         id, text = reader.read()
 
-        if text == "":
-            text = "0" * 48
+        if text == "^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@":
+            text = " " * 48 
 
         # Aktuelle Zeitstempel erstellen
         timestamp = datetime.datetime.now().strftime("%d.%m.%Y %X")
